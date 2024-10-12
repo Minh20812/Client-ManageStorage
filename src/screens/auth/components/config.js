d297a14f615d085d1c4e34f2594fc8e29d293e20
Minh20812 <blossworld12@gmail.com>
2024-10-12 15:57:49 +0700

    Initial

--- src/screens/auth/components/config.js
+++ src/screens/auth/components/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiSSiAYvPpK4-iEJd-_xUDKuBEd3mfTNg",
-  authDomain: "manage-storage.firebaseapp.com",
-  projectId: "manage-storage",
-  storageBucket: "manage-storage.appspot.com",
-  messagingSenderId: "919841100036",
-  appId: "1:919841100036:web:b8f27207dbcdc4ea148051",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
