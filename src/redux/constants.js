f1563487291b5258eeec6c3754c2fb09440260f0
Minh20812 <blossworld12@gmail.com>
2024-10-14 10:30:36 +0700

    change constants

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,4 +1,8 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const SUPPLIERS_URL = "/api/suppliers";
-export const INVENTORY_URL = "/api/inventory";
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://manage-storage.onrender.com"
+    : "";
+
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const SUPPLIERS_URL = `${BASE_URL}/api/suppliers`;
+export const INVENTORY_URL = `${BASE_URL}/api/inventory`;
